5675fdc4ef1888872234ef1312a477cf6cfe8f62
Atul0003214 <atul0003214@gmail.com>
2022-08-20 02:15:35 +0530

    Modified date formate

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from database.connect_database import *
 from database.query import query
 from flask_mail import Mail, Message
 import configparser
+from datetime import date
 
 app = Flask(__name__)  # creating the Flask class object
 app.config['SECRET_KEY'] = "thisissecretkey"
@@ -276,6 +277,8 @@ def logout():
 def apply_course():
     try:
         if request.method == 'POST':
+            today = date.today()
+            dateToday = today.strftime("%d/%m/%Y")
             course_name = request.form['scourse']
             std_first_name = request.form['fname']
             std_last_name = request.form['lname']
@@ -284,7 +287,7 @@ def apply_course():
             std_highest_qualification = request.form['hqual']
             create_table_query = "CREATE TABLE IF NOT EXISTS university_admission_system.Registered_Course_Table (course_name text,std_first_name text ,std_last_name text,std_father_name text,std_email text,std_qualification text,applied_date timestamp, application_id uuid PRIMARY KEY,status text);"
             # applied_course_query = f"INSERT INTO university_admission_system.Registered_Course_Table (course_name,std_first_name,std_last_name,std_father_name,std_email,std_qualification,applied_date,application_id,status) values('{course_name}','{std_first_name}','{std_last_name}','{std_father_name}','{std_email}','{std_highest_qualification}',toTimestamp(now()),uuid(),'Under Review');"
-            applied_course_query = f"INSERT INTO university_admission_system.Registered_Course_Table (course_name,std_first_name,std_last_name,std_father_name,std_email,std_qualification,applied_date,application_id,status) values('{course_name}','{std_first_name}','{std_last_name}','{std_father_name}','{std_email}','{std_highest_qualification}',new Date (<YYYY-mm-dd>),uuid(),'Under Review');"
+            applied_course_query = f"INSERT INTO university_admission_system.Registered_Course_Table (course_name,std_first_name,std_last_name,std_father_name,std_email,std_qualification,applied_date,application_id,status) values('{course_name}','{std_first_name}','{std_last_name}','{std_father_name}','{std_email}','{std_highest_qualification}',dateToday,uuid(),'Under Review');"
             LogDetails("create table query", create_table_query)
             create_table_dbobj = DatabaseConnection()
             insert_table_dbobj = DatabaseConnection()
